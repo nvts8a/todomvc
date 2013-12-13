ba1cec184117d8b1fcd2a5d00ed3477183a45af1
Steven Justin <nvts8a@gmail.com>
2013-12-13 17:20:01 -0500

    active route and render example

--- js/router.js
+++ js/router.js
@@ -1,6 +1,6 @@
 Todos.Router.map( function() { 
 	this.resource( "todos", { path: "/" }, function() { 
-		
+		this.route( "active" );		
 	});
 });
 
@@ -8,6 +8,17 @@ Todos.TodosRoute = Ember.Route.extend( {
 	model:	function() {
 				return this.store.find( "todo" );
 			}
+});
+
+Todos.TodosActiveRoute = Ember.Route.extend( {
+	model:			function() {
+						return this.store.filter( "todo", function( todo ) {
+							return !todo.get( "isCompleted" );
+						});
+					},
+	renderTemplate: function( controller ) {
+						this.render( "todos/index", { controller: controller } );
+					}
 });
 
 Todos.TodosIndexRoute = Ember.Route.extend( {
